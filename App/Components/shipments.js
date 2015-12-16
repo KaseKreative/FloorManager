5b0c63c6247cd5dafccd02623bbe072d69703ab6
Jimin Hong <jhong.dev@gmail.com>
2015-12-16 17:00:49 -0500

    changed the options in Shipments.js

--- App/Components/shipments.js
+++ App/Components/shipments.js
@@ -1,143 +1,65 @@
 'use strict';
 
 var React = require('react-native');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-	Component,
+  Component,
   TouchableHighlight,
-	Radio,
+  TouchableWithoutFeedback,
+  Radio,
 } = React;
 
-var Radio = require('../../node_modules/react-native-radio-button-classic');
-var Option = Radio.Option;
+import { RadioButtons, SegmentedControls } from 'react-native-radio-buttons';
 
-class Item extends Component {
-  constructor(props) {
-    super(props);
+// Main
+class Shipments extends React.Component {
+  constructor(){
+    super();
+    this.state = {};
   }
 
-  render() {
-    var { title, description } = this.props;
+  render(){
+
+    const options = [
+      "Pod 1",
+      "Pod 2"
+    ];
+
+    function setSelectedOption(selectedOption){
+      this.setState({
+        selectedOption
+      });
+    };
+
+    function renderOption(option, selected, onSelect, index){
+      const style = selected ? { fontWeight: 'bold'} : {}
+
+      return (
+        <TouchableWithoutFeedback onPress={onSelect} key={index}>
+          <Text style={style}>{option}</Text>
+        </TouchableWithoutFeedback>
+      );
+    }
+
+    function renderContainer(optionNodes){
+      return <View>{optionNodes}</View>;
+    }
 
     return (
-      <View style={{ paddingTop: 7, paddingLeft: 10 }}>
-        <Text style={styles.title}>{ title }</Text>
-        <Text style={styles.description}>{ description }</Text>
-      </View>
-    );
-  }
-}
-
-class Shipments extends React.Component {
-
-	constructor(props){
-    super(props);
-    this.state = {
-      buttonValue:'Move pod',
-      optionSelected:1,
-      sectionSelected:1,
-      showCancel: false, // sets flag for rendering renderCancel()
-      val1: 'A1-26',
-      val2: 'A1-27',
-      val3: 'A1-28',
-      val4: 'A1-29',
-    }
-  }
-
-  onSelect(index) {
-    this.setState({
-      optionSelected: index + 1,
-    })
-  }
-
-  onSelectSection(index){
-    this.setState({
-      sectionSelected: index + 1,
-    })
-  }
-
-  makeBtn(event) {
-      this.setState({
-          showCancel: !this.state.showCancel
-      });
-  }
-
-  render() {
-      return (
-        <View style={styles.container}>
-
-          
-
-          
-
-          <Radio onSelect={this.onSelect.bind(this)} defaultSelect={this.state.optionSelected - 1}>
-            <Option color="gray" selectedColor="#008BEF">
-              <Item title="Pod 1" description="Quantity : , Total Weight: "/>
-            </Option>
-            <Option color="gray" selectedColor="#008BEF">
-              <Item title="Pod 2" description="Quantity : , Total Weight: "/>
-            </Option>
-            <Option color="gray" selectedColor="#008BEF">
-              <Item title="Pod 3" description="Quantity : , Total Weight: "/>
-            </Option>
-          </Radio>
-
-          <Radio onSelect={this.onSelectSection.bind(this)} defaultSelect={this.state.sectionSelected - 1}>
-            <Option color="gray" selectedColor="#008BEF">
-              <Item title={this.state.val1} description=""/>
-            </Option>
-            <Option color="gray" selectedColor="#008BEF">
-              <Item title={this.state.val2} description=""/>
-            </Option>
-            <Option color="gray" selectedColor="#008BEF">
-              <Item title={this.state.val3} description=""/>
-            </Option>
-          </Radio>
-
-          <View style={{ paddingTop: 40 }}>
-            <Text>You have selected Pod {this.state.optionSelected}</Text>
-            <Text>Section: {this.state.sectionSelected}</Text>
-          </View>
-
-          <TouchableHighlight 
-          onPress={this.makeBtn.bind(this)}>
-            <View>
-              <Text>
-                Move the pod
-              </Text>
-            </View>
-          </TouchableHighlight>
-
-          {this.renderCancel()}
-
-        </View>
-      );
-  }
-renderCancel() {
-      if (this.state.showCancel) {
-        return (
-          
-          <View>
-          <TouchableHighlight
-            style={styles.button}
-            >
-                <View>
-                    <Text style={styles.welcome}>{this.state.buttonValue}</Text>
-                </View>
-          </TouchableHighlight>
-          </View>
-
-
-        );
-      }
-      else {
-          return (
-            <View></View>
-          );
-      }
+      <View style={{marginTop: 10, padding: 20, backgroundColor: 'white'}}>
+        <Text style={{paddingBottom: 10, fontWeight:'bold'}}>Select a pod to move.</Text>
+        <RadioButtons
+          options={ options }
+          onSelection={ setSelectedOption.bind(this) }
+          selectedOption={this.state.selectedOption }
+          renderOption={ renderOption }
+          renderContainer={ renderContainer }/>
+        <Text>Selected option: {this.state.selectedOption || 'none'}</Text>
+      </View>);
   }
 
 };
